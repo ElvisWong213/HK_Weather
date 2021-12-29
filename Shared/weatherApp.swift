13567aea45bf1137130b6d14e03ae6438f7c7e02
Stevewong2007082 <wongwingto2007082@yahoo.com>
2021-12-29 16:56:03 +0800

    Initial Commit

---
+++ Shared/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  Shared
+//
+//  Created by Steve on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
